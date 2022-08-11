d789362874424b1a1220a5657ebbb7d5f1516eef
JvD1993 <111055270+JvD1993@users.noreply.github.com>
2022-08-11 12:45:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,31 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+import requests
+from lxml.html import fromstring
+import lxml.html
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+file1 = open('YourPath/PeopleAlsoAsk/keywords.txt', 'r')
+file2 = open('YourPath/PeopleAlsoAsk/paa.txt', 'w')
+Lines = file1.readlines()
+header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
+        'Accept-Language': 'tr-tr,en;q=0.9',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+        }
+for line in Lines:
+        query = line.strip()
+        response = requests.get(f'https://www.google.com/search?q={query}&start=0', headers=header).text
+        tree = lxml.html.fromstring(response)
+        node = tree.xpath('//@data-q')
+        x = node[0]
+        y = node[1]
+        z = node[2]
+        print(query)
+        file2.write(query)
+        file2.write(": ")
+        file2.write(x)
+        file2.write(" , ")
+        file2.write(y)
+        file2.write(" , ")
+        file2.write(z)
+        file2.write("\n")
+file1.close()
+file2.close()
